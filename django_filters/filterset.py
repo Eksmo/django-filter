eb875c85a13c30d7f2a950e4bab74e95193c3b12
cephey <andrey.ptitsyn86@gmail.com>
2014-03-04 16:12:30 +0300

    Update filterset.py

--- django_filters/filterset.py
+++ django_filters/filterset.py
@@ -4,7 +4,7 @@ from django import forms
 from django.db import models
 from django.db.models.fields import FieldDoesNotExist
 from django.db.models.related import RelatedObject
-from django.db.models.sql.constants import LOOKUP_SEP
+from django.db.models.constants import LOOKUP_SEP
 from django.utils.datastructures import SortedDict
 from django.utils.text import capfirst
 
